fdf514ed36c07f0e002a7df03c350e3da6eed253
Lucid19 <daumantasdauksas9@gmail.com>
2022-07-19 21:31:24 +0100

    ss

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -2,29 +2,21 @@ const Markov = require("js-markov")
 const markov = new Markov()
 const config = require("../config.json")
 const cron = require("cron")
-const {client} = require("../index")
 
-const guild = client.guilds.cache.get(config.GuildID)
-
+var guild
+var consoleLog
 
 const maxChannels = 30
 const minText = 15
 const maxText = 100
 
-var sendMarkov = new cron.CronJob("0 0,15,30,45 * * * *", () => {
-    markov.train()
-    for(i=0; i >= maxChannels; i++){
-    let channel = guild.channels.cache.find(channel => channel.name === String(i))
-    channel.send(markov.generateRandom(Math.ceil(Math.random() * (maxText - minText)) +  minText))
-    }
-})
-
-sendMarkov.start()
-
 module.exports = {
     name: "messageCreate",
     async execute(message) {
-        var consoleLog = guild.channels.cache.get(config.consoleLogID)
+        // setting values on interaction
+        guild = message.guild
+        consoleLog = guild.channels.cache.get(config.consoleLogID)
+
         console.log("sex")
         try {
             markov.addStates(message.content)
@@ -35,3 +27,12 @@ module.exports = {
         }
     }
 }
+
+var sendMarkov = new cron.CronJob("0 0,15,30,45 * * * *", () => {
+    if(guild) { markov.train()
+    for(i=0; i >= maxChannels; i++){
+    let channel = guild.channels.cache.find(channel => channel.name === String(i))
+    channel.send(markov.generateRandom(Math.ceil(Math.random() * (maxText - minText)) +  minText))}}
+})
+
+sendMarkov.start()
