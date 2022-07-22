3602e20dfe1f17d03dc43fa2ade875c6b8872877
Lucid19 <daumantasdauksas9@gmail.com>
2022-07-22 19:55:59 +0100

    commit

--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "guildMemberAdd",
-
-    async execute(member) {
-        const channelNumber = String(Math.ceil(Math.random() * maxChannels))
-        channels.forEach((channel) => {if(channel.name === channelNumber) channel.permissionOverwrites.set([{id: member.user.id, allow: ["VIEW_CHANNEL"]}])})
-    }
-}
