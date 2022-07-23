e7c48ef9cd2226c52f4695473b531cec9d666c9d
Lucid19 <daumantasdauksas9@gmail.com>
2022-07-23 09:55:04 +0100

    commit

--- events/ready.js
+++ events/ready.js
@@ -5,12 +5,6 @@ const {Routes} = require("discord-api-types/v9");
 //jobs
 const cron = require("cron")
 
-// vars
-const maxChannels = 30
-
-var category
-var guild
-
 module.exports = {
     name : "ready",
     once : true,
@@ -19,10 +13,11 @@ module.exports = {
 
         // user
         const CLIENT_ID = client.user.id
-        guild = client.guilds.cache.get(process.env.GUILDID)
+        const guild = client.guilds.cache.get(process.env.GUILDID)
         
         // Channels
-        category = guild.channels.cache.get(process.env.LEVELID)
+        const maxChannels = 30
+        const category = guild.channels.cache.get(process.env.LEVELID)
 
         // REST API
         const rest = new REST({
@@ -40,26 +35,25 @@ module.exports = {
         catch(err){
             if(err)console.error(err)
         }
+
+        // starting up jobs
+        var autoGenerateChannels =  new cron.CronJob("0 53 * * * *", async () => {
+            const members = await guild.members.fetch()
+
+            category.children.forEach(channel => channel.delete())
+            for(let i = 0; i <= maxChannels; i++){
+                guild.channels.create(String(i), {
+                    type: "GUILD TEXT",
+                    parent: process.env.LEVELID,
+                    permissionOverwrites: [{id: process.env.GUILDID, deny: ["VIEW_CHANNEL"]}]
+            })}
+
+            const channels = await guild.channels.fetch()
+
+            members.forEach((member) => {
+                const channelNumber = String(Math.ceil(Math.random() * maxChannels))
+                channels.forEach((channel) => {if(channel.name === channelNumber) channel.permissionOverwrites.set([{id: member.user.id, allow: ["VIEW_CHANNEL"]}])})
+            })})
+        autoGenerateChannels.start()
 }
 }
-
-// starting up jobs
-var autoGenerateChannels =  new cron.CronJob("0 53 * * * *", async () => {
-    const members = await guild.members.fetch()
-
-    category.children.forEach(channel => channel.delete())
-    for(let i = 0; i <= maxChannels; i++){
-        guild.channels.create(String(i), {
-            type: "GUILD TEXT",
-            parent: levelID,
-            permissionOverwrites: [{id: process.env.GUILDID, deny: ["VIEW_CHANNEL"]}]
-    })}
-
-    const channels = await guild.channels.fetch()
-
-    members.forEach((member) => {
-        const channelNumber = String(Math.ceil(Math.random() * maxChannels))
-        channels.forEach((channel) => {if(channel.name === channelNumber) channel.permissionOverwrites.set([{id: member.user.id, allow: ["VIEW_CHANNEL"]}])})
-    })})
-
-autoGenerateChannels.start()
