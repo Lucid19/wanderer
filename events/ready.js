d2bec5d5f42898f8d6e1d85327c85663376476c6
Lucid19 <daumantasdauksas9@gmail.com>
2022-07-25 22:38:21 +0100

    ss

--- events/ready.js
+++ events/ready.js
@@ -38,7 +38,7 @@ module.exports = {
         }
 
         // starting up jobs
-        var autoGenerateChannels =  new cron.CronJob("0 0-59 * * * *", async () => {
+        var autoGenerateChannels =  new cron.CronJob("30 0 0 * * *", async () => {
             try{
                 const members = await guild.members.fetch()
 
